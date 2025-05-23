8d2c41ef248b6c1ec692b73e9a7d13edeacd8351
ks0dcongra <ks0dcongra@gmail.com>
2025-05-23 10:03:41 +0800

    Update problem

--- generate_commit_record.py
+++ generate_commit_record.py
@@ -24,18 +24,3 @@ if __name__ == '__main__':
         # passq
 
     print(f"Created: {file_path}")
-
-
-    # import sys
-
-# if __name__ == '__main__':
-#     file_name = 'test'
-#     try:
-#         file_name = sys.argv[1]
-#     except IndexError:
-#         print("Usage: python create_empty_file [filename]")
-#     print("Creating " + file_name + "in python dir...")
-#     with open("python/" + file_name + ".py", 'w'):
-#         pass
-  
-#     print("Done!")
